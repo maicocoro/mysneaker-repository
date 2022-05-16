f22e3f3c9008fed21179ee07b73fc68c9ed8c219
maicocoro <maycocoa00005@gmail.com>
2022-05-16 19:32:56 +0900

    first-commit

---
+++ js/main.js
@@ -0,0 +1,56 @@
+//ハンバーガーメニュー
+$(function(){
+    $(".humburger-btn").on("click",function(){
+        $(".humburger-menu").toggleClass("show");
+    })
+})
+//フェードイン
+$(function () {
+    $('.skill-text,.service-item').on('inview',function(){
+      $(this).addClass('fadeIn')
+    });
+   });
+      
+
+//画像スライダー
+$(function () {
+    $('.slider1').slick({
+      autoplay:true,
+      autoplaySpeed:0,
+      speed:3000,
+      cssEase:"linear",
+      slidesToShow:4,
+      swipe: false,
+      arrows:false,
+      responsive: [
+        {
+          breakpoint: 599,
+          settings: {
+            slidesToShow:3,
+          }
+        }
+      ]
+    })
+   })
+   
+   $(function () {
+    $('.slider2').slick({
+      autoplay:true,
+      autoplaySpeed:0,
+      speed:3000,
+      cssEase:"linear",
+      slidesToShow:4,
+      swipe: false,
+      arrows:false,
+      rtl: true,
+      responsive: [
+        {
+          breakpoint: 599,
+          settings: {
+            slidesToShow:2.5,
+          }
+        }
+      ]
+    })
+   })
+   
